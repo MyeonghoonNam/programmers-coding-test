e244bb725a8799e5ef59a4f83bc2191c2fc545f6
MyeonghoonNam <codenmh0822@naver.com>
2021-06-30 19:20:39 +0900

    문제 해결

---
+++ 연습 문제/약수의합.js
@@ -0,0 +1,19 @@
+const n = 12;
+
+console.log(solution(n));
+
+function solution(n) {
+  const result = [];
+
+  if (n === 0) return 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      result.push(i);
+    }
+  }
+
+  return result.reduce((acc, cur) => {
+    return acc + cur;
+  });
+}
