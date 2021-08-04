7f509cf31ad262249ee8d8d58c1a556a0bfa11f7
MyeongHoonNam <codenmh0822@naver.com>
2021-08-04 19:57:14 +0900

    문제 해결 - 클래스를 활용한 큐 구현 방법

--- 스택과 큐/프린터.js
+++ 스택과 큐/프린터.js
@@ -1,40 +1,104 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if(this.head === null) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  dequeue() {
+    const value = this.head.value;
+
+    this.head = this.head.next;
+
+    return value;
+  }
+
+  peek() {
+    return this.head.value;
+  }
+}
+
+function solution(priorities, location) {
+  const queue = new Queue();
+
+  for(let i = 0; i < priorities.length; i++) {
+    queue.enqueue([priorities[i], i]);
+  }
+
+  priorities.sort((a, b) => b - a);
+  
+  let count = 0;
+  while(true) {
+    const curValue = queue.peek();
+
+    if(curValue[0] < priorities[count]) {
+      queue.enqueue(queue.dequeue());
+    } else {
+      const value = queue.dequeue();
+      count++;
+
+      if(location === value[1]) return count;
+    }
+  }
+}
+
+
 const priorities = [2, 1, 3, 2];
 const location = 2;
 
 console.log(solution(priorities, location));
 
-function solution(priorities, location) {
-  let myDoc = location;
-  let result = 0;
 
-  while (true) {
-    const curDoc = priorities.shift();
 
-    let flag = true;
+// function solution(priorities, location) {
+//   let myDoc = location;
+//   let result = 0;
 
-    for (let i = 0; i < priorities.length; i++) {
-      if (priorities[i] > curDoc) {
-        priorities.push(curDoc);
-        flag = false;
-        break;
-      }
-    }
+//   while (true) {
+//     const curDoc = priorities.shift();
 
-    if (flag) {
-      result++;
+//     let flag = true;
 
-      if (myDoc === 0) {
-        return result;
-      }
-    }
+//     for (let i = 0; i < priorities.length; i++) {
+//       if (priorities[i] > curDoc) {
+//         priorities.push(curDoc);
+//         flag = false;
+//         break;
+//       }
+//     }
 
-    myDoc--;
+//     if (flag) {
+//       result++;
 
-    if (myDoc === -1) {
-      myDoc = priorities.length - 1;
-    }
-  }
-}
+//       if (myDoc === 0) {
+//         return result;
+//       }
+//     }
+
+//     myDoc--;
+
+//     if (myDoc === -1) {
+//       myDoc = priorities.length - 1;
+//     }
+//   }
+// }
 
 // findIndex 메소드 활용법
 
