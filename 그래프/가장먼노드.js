c25279394984acdfcbdfce46382618d4fb9326ef
MyeongHoonNam <codenmh0822@naver.com>
2021-08-06 15:43:21 +0900

    문제 복습 - 큐 클래스 직접 작성하여 구현

--- 그래프/가장먼노드.js
+++ 그래프/가장먼노드.js
@@ -1,8 +1,3 @@
-const n = 6;
-const vertex = 	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]];
-
-console.log(solution(n, vertex));
-
 // function solution(n, vertex) {
 //   const graph = Array.from(new Array(n + 1), () => new Array());
 //   const visited = new Array(n + 1).fill(false);
@@ -50,34 +45,101 @@ console.log(solution(n, vertex));
 // }
 
 // 문제 복습
+// function solution(n, edge) {
+//   const graph = Array.from(Array(n + 1), () => []); // 인접리스트
+
+//   for(let i = 0; i < edge.length; i++) {
+//     const [src, dest] = edge[i];
+
+//     graph[src].push(dest);
+//     graph[dest].push(src);
+//   }
+
+//   const distance = Array(n + 1).fill(0);
+//   distance[1] = 1; // 1번 노드 부터 시작하여 갯수 측정
+
+//   // BFS
+//   const queue = [1];
+
+//   while(queue.length) {
+//     const src = queue.shift(); // 시간복잡도가 O(n)이지만 요소가 적을 경우 자바스크립트 엔진에서 최적화를 해준다.
+
+//     for(const dest of graph[src]) {
+//       if(distance[dest] === 0) {
+//         queue.push(dest);
+//         distance[dest] = distance[src] + 1;
+//       }
+//     }
+//   }
+
+//   const max = Math.max(...distance);
+
+//   return distance.filter(item => item === max).length;
+// }
+
+// 문제 복습 - 큐 클래스 구현
+// 요소의 개수가 적다면 shift 구현도 지장없지만 요소의 개수가 많다면 직접 큐를 구현하자.
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+
+  dequeue() {
+    const value = this.queue[this.front];
+
+    delete this.queue[this.front];
+
+    this.front++;
+
+    return value;
+  }
+
+  isEmpty() {
+    return this.rear === this.front;
+  }
+}
+
 function solution(n, edge) {
   const graph = Array.from(Array(n + 1), () => []); // 인접리스트
-
+  
   for(let i = 0; i < edge.length; i++) {
     const [src, dest] = edge[i];
-
+  
     graph[src].push(dest);
     graph[dest].push(src);
   }
-
+  
   const distance = Array(n + 1).fill(0);
   distance[1] = 1; // 1번 노드 부터 시작하여 갯수 측정
-
+  
   // BFS
-  const queue = [1];
+  const queue = new Queue();
+  
+  queue.enqueue(1);
 
-  while(queue.length) {
-    const src = queue.shift(); // 시간복잡도가 O(n)이지만 요소가 적을 경우 자바스크립트 엔진에서 최적화를 해준다.
-
+  while(!queue.isEmpty()) {
+    const src = queue.dequeue(); 
+  
     for(const dest of graph[src]) {
       if(distance[dest] === 0) {
-        queue.push(dest);
+        queue.enqueue(dest);
         distance[dest] = distance[src] + 1;
       }
     }
   }
-
+  
   const max = Math.max(...distance);
-
+  
   return distance.filter(item => item === max).length;
 }
+
+const n = 6;
+const vertex = 	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]];
+
+console.log(solution(n, vertex));
