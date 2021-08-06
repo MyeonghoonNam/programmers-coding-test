4d3d8cbd9cb82a2f61ea1f02e20874a5ffdc1648
MyeongHoonNam <codenmh0822@naver.com>
2021-08-06 15:22:51 +0900

    문제 해결 - Map과 고차함수의 활용

--- 해시/베스트앨범.js
+++ 해시/베스트앨범.js
@@ -1,36 +1,57 @@
 const genres = ['classic', 'pop', 'classic', 'classic', 'pop'];
 const plays = [500, 600, 150, 800, 2500];
 
-console.log(Solution(genres, plays));
+console.log(solution(genres, plays));
 
-function Solution(genres, plays) {
-  const list = genres.reduce((hash, g, i) => {
-    if (!hash[g]) {
-      hash[g] = { plays: plays[i], music: [] };
-    } else {
-      hash[g].plays += plays[i];
-    }
-    hash[g].music.push([i, plays[i]]);
-    return hash;
-  }, {});
+// function solution(genres, plays) {
+//   const list = genres.reduce((hash, g, i) => {
+//     if (!hash[g]) {
+//       hash[g] = { plays: plays[i], music: [] };
+//     } else {
+//       hash[g].plays += plays[i];
+//     }
+//     hash[g].music.push([i, plays[i]]);
+//     return hash;
+//   }, {});
 
-  const sortedList = Object.values(list).sort((a, b) => {
-    return b.plays - a.plays;
-  });
+//   const sortedList = Object.values(list).sort((a, b) => {
+//     return b.plays - a.plays;
+//   });
 
-  const result = [];
-  sortedList.forEach((g) => {
-    if (g.music.length > 1) {
-      g.music.sort((a, b) => {
-        return b[1] - a[1] || a[0] - b[0];
+//   const result = [];
+//   sortedList.forEach((g) => {
+//     if (g.music.length > 1) {
+//       g.music.sort((a, b) => {
+//         return b[1] - a[1] || a[0] - b[0];
+//       });
+
+//       result.push(g.music[0][0]);
+//       result.push(g.music[1][0]);
+//     } else {
+//       result.push(g.music[0][0]);
+//     }
+//   });
+
+//   return result;
+// }
+
+// Map을 활용하여 풀이
+function solution(genres, plays) {
+  const genreMap = new Map();
+
+  genres
+    .map((genre, index) => [genre, plays[index]])
+    .forEach(([genre, play], index) => {
+      const data = genreMap.get(genre) || {total:0, songs:[]};
+
+      genreMap.set(genre, {
+        total : data.total + play,
+        songs: [...data.songs, { play, index}].sort((a, b) => b.play - a.play).slice(0, 2)
       });
-
-      result.push(g.music[0][0]);
-      result.push(g.music[1][0]);
-    } else {
-      result.push(g.music[0][0]);
-    }
   });
 
-  return result;
+  return [...genreMap.entries()]
+    .sort((a, b) => b[1].total - a[1].total)
+    .flatMap(item => item[1].songs)
+    .map(song => song.index);
 }
