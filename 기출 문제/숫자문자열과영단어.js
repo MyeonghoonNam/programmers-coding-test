0e8e887788c3779205a039267363bfe2ca181856
MyeongHoonNam <codenmh0822@naver.com>
2021-11-14 17:05:44 +0900

    문제 해결

--- 기출 문제/숫자문자열과영단어.js
+++ 기출 문제/숫자문자열과영단어.js
@@ -1,53 +1,21 @@
-const s = 'one4oneeight';
+const solution = (s) => {
+  let result = 0;
 
+  s = s.replace(/zero/g, 0);
+  s = s.replace(/one/g, 1);
+  s = s.replace(/two/g, 2);
+  s = s.replace(/three/g, 3);
+  s = s.replace(/four/g, 4);
+  s = s.replace(/five/g, 5);
+  s = s.replace(/six/g, 6);
+  s = s.replace(/seven/g, 7);
+  s = s.replace(/eight/g, 8);
+  s = s.replace(/nine/g, 9);
+
+  result = Number(s);
+
+  return result;
+};
+
+const s = 'onetwothreeone';
 console.log(solution(s));
-
-function solution(s) {
-  const obj = {
-    zero: 0,
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-  };
-
-  const splitStr = (s) => {
-    let startIdx = 0;
-    let lastIdx = 3;
-
-    while (startIdx < s.length) {
-      let str = s.slice(startIdx, lastIdx);
-      if (obj[str]) {
-        result.push(obj[str]);
-
-        startIdx += lastIdx;
-        lastIdx = 3;
-      } else {
-        lastIdx++;
-      }
-    }
-  };
-
-  const result = [];
-
-  const reg = /([a-z]+)|\d/g;
-
-  s = s.match(reg);
-
-  for (let i = 0; i < s.length; i++) {
-    if (!Number.isInteger(Number(s[i])) && s[i].length < 6) {
-      result.push(obj[s[i]]);
-    } else if (Number.isInteger(Number(s[i]))) {
-      result.push(Number(s[i]));
-    } else {
-      splitStr(s[i]);
-    }
-  }
-
-  return Number(result.join(''));
-}
