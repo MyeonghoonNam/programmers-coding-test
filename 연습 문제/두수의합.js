b35921aef32db039af776faa4effff292f8ddf60
MyeongHoonNam <codenmh0822@naver.com>
2022-11-04 20:39:07 +0900

    Day1 문제 해결

---
+++ 연습 문제/두수의합.js
@@ -0,0 +1,7 @@
+function solution(num1, num2) {
+  return num1 + num2;
+}
+
+const num1 = 2;
+const num2 = 3;
+console.log(solution(num1, num2));
---
+++ 연습 문제/몫구하기.js
@@ -0,0 +1,8 @@
+function solution(num1, num2) {
+  return parseInt(num1 / num2);
+}
+
+const num1 = 10;
+const num2 = 5;
+
+console.log(solution(num1, num2));
