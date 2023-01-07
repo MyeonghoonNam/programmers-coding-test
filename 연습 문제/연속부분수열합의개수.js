7a38e2f0caa871ba919c2dca99bf893af9278d2f
MyeongHoonNam <codenmh0822@naver.com>
2023-01-07 15:03:11 +0900

    문제 해결

---
+++ 연습 문제/연속부분수열합의개수.js
@@ -0,0 +1,18 @@
+const solution = (elements) => {
+  const circular = elements.concat(elements);
+  const set = new Set();
+
+  for (let i = 0; i < elements.length; i++) {
+    let sum = 0;
+
+    for (let j = 0; j < elements.length; j++) {
+      sum += circular[i + j];
+      set.add(sum);
+    }
+  }
+
+  return set.size;
+};
+
+const elements = [7, 9, 1, 1, 4];
+console.log(solution(elements));
