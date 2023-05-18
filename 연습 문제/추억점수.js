334a16bf16d46970c78883bf6468e1fc63b2060c
MyeongHoonNam <codenmh0822@naver.com>
2023-05-19 01:25:25 +0900

    문제 2차 해결 - 고차 함수 적용

--- 연습 문제/추억점수.js
+++ 연습 문제/추억점수.js
@@ -1,34 +1,45 @@
+// 2차 해결
+// 고차 함수 적용
 function solution(name, yearning, photo) {
-  const N = name.length;
-  const P = photo.length;
-  const map = new Map();
-  const result = [];
-
-  for (let i = 0; i < N; i++) {
-    const person = name[i];
-    const value = yearning[i];
-
-    map.set(person, value);
-  }
-
-  for (let i = 0; i < P; i++) {
-    let sum = 0;
-
-    for (let j = 0; j < photo[i].length; j++) {
-      const person = photo[i][j];
-
-      if (map.get(person)) {
-        sum += map.get(person);
-      } else {
-        map.set(person, 0);
-      }
-    }
-
-    result.push(sum);
-  }
+  const result = photo.map((p) =>
+    p.reduce((acc, cur) => (acc += yearning[name.indexOf(cur)] ?? 0), 0)
+  );
 
   return result;
 }
+
+// 1차 해결
+// function solution(name, yearning, photo) {
+//   const N = name.length;
+//   const P = photo.length;
+//   const map = new Map();
+//   const result = [];
+
+//   for (let i = 0; i < N; i++) {
+//     const person = name[i];
+//     const value = yearning[i];
+
+//     map.set(person, value);
+//   }
+
+//   for (let i = 0; i < P; i++) {
+//     let sum = 0;
+
+//     for (let j = 0; j < photo[i].length; j++) {
+//       const person = photo[i][j];
+
+//       if (map.get(person)) {
+//         sum += map.get(person);
+//       } else {
+//         map.set(person, 0);
+//       }
+//     }
+
+//     result.push(sum);
+//   }
+
+//   return result;
+// }
 
 const name = ["may", "kein", "kain", "radi"];
 const yearning = [5, 10, 1, 3];
