4778c0c509ad80db67fe5a49774bc3c785fbbc0d
MyeonghoonNam <codenmh0822@naver.com>
2024-04-25 22:56:59 +0900

    문제 2차 해결

--- 연습 문제/문자열묶기.js
+++ 연습 문제/문자열묶기.js
@@ -2,20 +2,42 @@ const strArr = ["a", "bc", "d", "efg", "hi"];
 
 console.log(solution(strArr));
 
+// 문제 2차 해결
+// Object 활용
 function solution(strArr) {
-  const map = new Map();
+  const obj = {};
 
   for (let str of strArr) {
     const strLen = str.length;
 
-    if (map.has(strLen)) {
-      map.set(strLen, [...map.get(strLen), str]);
+    if (obj[strLen]) {
+      obj[strLen] = [...obj[strLen], str];
     } else {
-      map.set(strLen, [str]);
+      obj[strLen] = [str];
     }
   }
 
-  const result = Math.max(...[...map.values()].map((v) => v.length));
+  const result = Math.max(...Object.values(obj).map((v) => v.length));
 
   return result;
 }
+
+// 문제 1차 해결
+// Map 활용
+// function solution(strArr) {
+//   const map = new Map();
+
+//   for (let str of strArr) {
+//     const strLen = str.length;
+
+//     if (map.has(strLen)) {
+//       map.set(strLen, [...map.get(strLen), str]);
+//     } else {
+//       map.set(strLen, [str]);
+//     }
+//   }
+
+//   const result = Math.max(...[...map.values()].map((v) => v.length));
+
+//   return result;
+// }
