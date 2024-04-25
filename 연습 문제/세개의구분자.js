214193a21aca8ebc61bc2ddab62b78d35d19665a
MyeonghoonNam <codenmh0822@naver.com>
2024-04-25 23:17:21 +0900

    문제 2차 해결 - 정규식

--- 연습 문제/세개의구분자.js
+++ 연습 문제/세개의구분자.js
@@ -1,8 +1,17 @@
-const myStr = "cabab";
+const myStr = "baconlettucetomato";
 
 console.log(solution(myStr));
 
+// 문제 2차 해결
+// 정규식
 function solution(myStr) {
-  const result = myStr.split(/[abc]/g).filter(Boolean);
-  return result.length ? result : ["EMPTY"];
+  const result = myStr.match(/[^a-c]+/g) || ["EMPTY"];
+  return result;
 }
+
+// 문제 1차 해결
+// split
+// function solution(myStr) {
+//   const result = myStr.split(/[abc]/g).filter(Boolean);
+//   return result.length ? result : ["EMPTY"];
+// }
