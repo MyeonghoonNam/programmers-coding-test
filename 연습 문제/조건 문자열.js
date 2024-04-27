e0931aa545fc8794ae00b36535a3c623613b42d0
MyeonghoonNam <codenmh0822@naver.com>
2024-04-27 23:08:36 +0900

    문제 2차 해결

--- 연습 문제/조건 문자열.js
+++ 연습 문제/조건 문자열.js
@@ -5,22 +5,38 @@ const m = 50;
 
 console.log(solution(ineq, eq, n, m));
 
+// 문제 2차 해결
+// 조건 로직 가시화
 function solution(ineq, eq, n, m) {
-  switch (eq) {
-    case "=": {
-      if (ineq === "<") {
-        return n <= m ? 1 : 0;
-      } else if (ineq === ">") {
-        return n >= m ? 1 : 0;
-      }
-    }
+  const operations = {
+    ">=": (n, m) => n >= m,
+    "<=": (n, m) => n <= m,
+    ">!": (n, m) => n > m,
+    "<!": (n, m) => n < m,
+  };
 
-    case "!": {
-      if (ineq === "<") {
-        return n < m ? 1 : 0;
-      } else if (ineq === ">") {
-        return n > m ? 1 : 0;
-      }
-    }
-  }
+  const result = Number(operations[ineq + eq](n, m));
+
+  return result;
 }
+
+// 문제 1차 해결
+// function solution(ineq, eq, n, m) {
+//   switch (eq) {
+//     case "=": {
+//       if (ineq === "<") {
+//         return n <= m ? 1 : 0;
+//       } else if (ineq === ">") {
+//         return n >= m ? 1 : 0;
+//       }
+//     }
+
+//     case "!": {
+//       if (ineq === "<") {
+//         return n < m ? 1 : 0;
+//       } else if (ineq === ">") {
+//         return n > m ? 1 : 0;
+//       }
+//     }
+//   }
+// }
