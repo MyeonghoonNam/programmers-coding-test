347bae7dab5ac47534ff8a1179033555fa21af5a
MyeonghoonNam <codenmh0822@naver.com>
2024-04-30 14:01:39 +0900

    문제 2차 해결 - 정규식

--- 연습 문제/배열 만들기 2.js
+++ 연습 문제/배열 만들기 2.js
@@ -3,13 +3,13 @@ const r = 555;
 
 console.log(solution(l, r));
 
+// 문제 2차 해결
+// 정규식
 function solution(l, r) {
   const result = [];
 
   for (let i = l; i <= r; i += 1) {
-    const filter = String(i)
-      .split("")
-      .every((e) => e === "0" || e === "5");
+    const filter = !/[^05]/.test(i);
 
     if (filter) {
       result.push(i);
@@ -18,3 +18,21 @@ function solution(l, r) {
 
   return result.length ? result : [-1];
 }
+
+// 문제 1차 해결
+// every 조건 분기
+// function solution(l, r) {
+//   const result = [];
+
+//   for (let i = l; i <= r; i += 1) {
+//     const filter = String(i)
+//       .split("")
+//       .every((e) => e === "0" || e === "5");
+
+//     if (filter) {
+//       result.push(i);
+//     }
+//   }
+
+//   return result.length ? result : [-1];
+// }
